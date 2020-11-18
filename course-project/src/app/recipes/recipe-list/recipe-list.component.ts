1d85f9798992ea1016c8e4907130257488de8d61
Carolista <cjones.renaissancewoman@gmail.com>
2020-11-18 15:23:37 -0600

    added content to page with Bootstrap stying for adding a new recipe and listing recipes, without functionality yet - segment 52

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fen%2Fphoto%2F1557385&psig=AOvVaw1Ebicm-M_leRtlFCndQM29&ust=1605820560521000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCip_mBje0CFQAAAAAdAAAAABAD')
+    ];
 
   constructor() { }
 
