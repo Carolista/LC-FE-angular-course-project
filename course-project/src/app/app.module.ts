1df6b41ca0114e3f53988c962b373ad037f019be
Carolista <cjones.renaissancewoman@gmail.com>
2020-11-17 22:22:32 -0600

    created and organized misc components

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { EditShoppingListComponent } from './shopping/shopping-list/edit-shopping-list/edit-shopping-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    EditShoppingListComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
