047b9291db263dd6b8c4aaa8a73b5d151e17f43b
Carolista <cjones.renaissancewoman@gmail.com>
2020-12-08 16:49:49 -0600

    used EventEmitter and ngIf to toggle display of recipes and shopping list from nav bar in header

--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -9,9 +9,14 @@ export class HeaderComponent implements OnInit {
 
     collapsed: boolean = true;
 
+    @Output() featureSelected = new EventEmitter<string>();
+
     constructor() { }
 
     ngOnInit(): void {
     }
 
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }
