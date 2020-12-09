bfa0c7034e6dec00ed6485595aa09e7cd704b13c
Carolista <cjones.renaissancewoman@gmail.com>
2020-12-08 19:42:24 -0600

    added event and property binding to pass selected recipe to recipe-details component for detailed display

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,14 +8,21 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYFfO-_d-sS_TTAqz0ROkbtJluuq89gwCv_g&usqp=CAU'),
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYFfO-_d-sS_TTAqz0ROkbtJluuq89gwCv_g&usqp=CAU')
+        new Recipe('Another Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYFfO-_d-sS_TTAqz0ROkbtJluuq89gwCv_g&usqp=CAU')
     ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
+    }
+    
 
 }
